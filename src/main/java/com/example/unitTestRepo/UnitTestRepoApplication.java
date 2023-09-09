fa1fc19a6456411a05a18cb81141515bd06bb9f4
bhavya-i-0707 <bhavya.i.0707@gmail.com>
2023-09-09 13:21:13 -0500

    First Commit

---
+++ src/main/java/com/example/unitTestRepo/UnitTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.unitTestRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/unitTestRepo/UnitTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unitTestRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
